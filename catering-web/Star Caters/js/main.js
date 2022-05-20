44b66c8832a304cd3f2c975a6d76efd16fc63161
Ahmed Faraz Ali <farazali506@gmail.com>
2022-05-20 10:41:48 +0500

    Web Oel

---
+++ catering-web/Star Caters/js/main.js
@@ -0,0 +1,134 @@
+(function ($) {
+  "use strict";
+  // Tutorial - https://youtu.be/VufN46OyFng
+
+  // Targetting all classes & id from HTML
+
+  let id = (id) => document.getElementById(id);
+
+  let classes = (classes) => document.getElementsByClassName(classes);
+
+  let username = id("username"),
+    email = id("email"),
+    date = id("date"),
+    form = id("form"),
+    select = id("select"),
+    special= id("special"),
+    errorMsg = classes("error")
+  // Adding the submit event Listener
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    engine(username, 0, "Username cannot be blank");
+    engine(email, 1, "Email cannot be blank");
+    engine(date, 2, "date cannot be blank");
+    engine(select, 3, "No of people cannot be blank");
+    engine(special, 4, "Special Request cannot be blank");
+
+  });
+
+  // engine function which will do all the works
+
+  let engine = (id, serial, message) => {
+    if (id.value.trim() === "") {
+     
+      id.style.border = "2px solid red";
+
+      
+    } else {
+      id.style.border = "2px solid green";
+
+    }
+  };
+
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
+      }
+    }, 1);
+  };
+  spinner();
+
+  // Initiate the wowjs
+  new WOW().init();
+
+  // Sticky Navbar
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 45) {
+      $(".navbar").addClass("sticky-top shadow-sm");
+    } else {
+      $(".navbar").removeClass("sticky-top shadow-sm");
+    }
+  });
+
+  // Dropdown on mouse hover
+  const $dropdown = $(".dropdown");
+  const $dropdownToggle = $(".dropdown-toggle");
+  const $dropdownMenu = $(".dropdown-menu");
+  const showClass = "show";
+
+  $(window).on("load resize", function () {
+    if (this.matchMedia("(min-width: 992px)").matches) {
+      $dropdown.hover(
+        function () {
+          const $this = $(this);
+          $this.addClass(showClass);
+          $this.find($dropdownToggle).attr("aria-expanded", "true");
+          $this.find($dropdownMenu).addClass(showClass);
+        },
+        function () {
+          const $this = $(this);
+          $this.removeClass(showClass);
+          $this.find($dropdownToggle).attr("aria-expanded", "false");
+          $this.find($dropdownMenu).removeClass(showClass);
+        }
+      );
+    } else {
+      $dropdown.off("mouseenter mouseleave");
+    }
+  });
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
+
+  // Facts counter
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 2000,
+  });
+
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    center: true,
+    margin: 24,
+    dots: true,
+    loop: true,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      },
+    },
+  });
+})(jQuery);
